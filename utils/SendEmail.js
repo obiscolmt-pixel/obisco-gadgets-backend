8cf7043fc38b4ed67415d2b0c414672f194b1c3e
ariogba patrick <obiscolmt@gmail.com>
2026-05-02 14:00:45 +0100

    switch to Brevo SMTP

--- utils/SendEmail.js
+++ utils/SendEmail.js
@@ -2,20 +2,17 @@ import nodemailer from 'nodemailer'
 
 const sendEmail = async ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+    host: 'smtp-relay.brevo.com',
+    port: 587,
+    secure: false,
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-    tls: {
-      rejectUnauthorized: false,
+      user: process.env.BREVO_SMTP_USER,
+      pass: process.env.BREVO_SMTP_PASS,
     },
   })
 
   const info = await transporter.sendMail({
-    from: `"OBISCO Gadgets" <${process.env.EMAIL_USER}>`,
+    from: `"OBISCO Gadgets" <obiscolmt@gmail.com>`,
     to,
     subject,
     html,
